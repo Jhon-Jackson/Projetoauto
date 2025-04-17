ea702c0fda47c6ce5203c82f4d1561c6b7f321e6
Jhon Jackson S S <radicalug@gmail.com>
2025-04-17 10:45:03 -0300

    03

--- pythonProject1/automoção.py
+++ pythonProject1/automoção.py
@@ -4,11 +4,15 @@ import time
 pyautogui.PAUSE = 2.5
 time.sleep(5)
 
-pyautogui.position()
-print(pyautogui.position())
+# input('enter')
+# pyautogui.position()
+# print(pyautogui.position())
 
 # time.sleep(5)
 #
 # pyautogui.press('b')
 
 # S = Point(x=1816, y=61)
+#Point(x=1742, y=49),Point(x=1878, y=148)
+# w = 136 h = 99
+img = pyautogui.screenshot('my.foto.png', region=(1742,55, 136,99))
--- pythonProject1/testes.py
+++ pythonProject1/testes.py
@@ -9,22 +9,14 @@ from autopw.funçoes import *
 pyautogui.PAUSE = 1.5
 time.sleep(4)
 
-img3 = r'C:\Users\Jhowzera\PycharmProjects\CursoemVideo\Projetoauto\pythonProject1\autopw\imagenss\sul.png'
+img3 = r'C:\Users\Jhowzera\PycharmProjects\CursoemVideo\Projetoauto\pythonProject1\autopw\imagenss\my.foto.png'
+local = pyautogui.locateOnScreen(img3, confidence=0.5, region=(1742,55, 136,99))
+if local:
+    x, y = pyautogui.center(local)
 
-tempo_limite = 30
-tempo_inicial = time.time()
-imagem_encontrada = False
-time.sleep(1)
-while not imagem_encontrada and time.time() - tempo_inicial < tempo_limite:
-    try:
-      x, y = pyautogui.locateCenterOnScreen(img3, confidence=0.8) #region=(0,0, w, h))
-      pyautogui.moveTo(x, y)
-      print("Imagem encontrada e clicada!")
-      imagem_encontrada = True
-    except pyautogui.ImageNotFoundException:
-      time.sleep(1)
-      print('Imagem não encontrada. Tentando novamente...')
+    pyautogui.moveTo(x, y)
 
-    except Exception as e:
-      print(f"Ocorreu um erro: {e}")
-      break
+    print('Imagem encontrada nas coordenadas:', x, y)
+
+
+
